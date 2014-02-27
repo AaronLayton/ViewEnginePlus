aaf7b3cd4818c5d24d45061f2aae9b034a440a83
Aaron layton <aaron.r.layton@gmail.com>
2014-02-27 23:09:25 +0000

    Added some test cases, caching issue

--- ViewEnginePlus.Demo/Global.asax.cs
+++ ViewEnginePlus.Demo/Global.asax.cs
@@ -18,6 +18,38 @@ namespace ViewEnginePlus.Demo
         {
             AreaRegistration.RegisterAllAreas();
 
+            // Clear all old ViewEngines and register our RazorThemeViewEngine
+            System.Web.Mvc.ViewEngines.Engines.Clear();
+
+            System.Web.Mvc.ViewEngines.Engines.Add(new RazorViewEnginePlus());
+
+            ViewEnginePlus.AddViewRule("aaron", () =>
+            {
+                try
+                {
+                    return HttpContext.Current.Request.QueryString["testcase"] == "aaron" ? true : false;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            });
+
+            ViewEnginePlus.AddViewRule("debug", () => {
+                try
+                {
+                    var returnVal = false;
+
+                    if (HttpContext.Current.Request.IsLocal) returnVal = true;
+
+                    return returnVal;
+                } catch(Exception e) {
+                    return false;
+                }
+            });
+
+            
+
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
--- ViewEnginePlus.Demo/ViewEnginePlus/ViewEnginePlus.cs
+++ ViewEnginePlus.Demo/ViewEnginePlus/ViewEnginePlus.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +9,45 @@ namespace ViewEnginePlus
 {
     public class ViewEnginePlus //: IViewEngine
     {
+        public static String viewCase;
+        public static String folderCase;
+        private static List<ViewEnginePlusCase> testCases = new List<ViewEnginePlusCase>();
 
+        public static void AddViewRule(string stateName, Func<bool> testCase)
+        {
+            var engineCase = new ViewEnginePlusCase();
+            engineCase.name = stateName;
+            engineCase.caseFunction = testCase;
+
+            testCases.Add(engineCase);
+        }
+
+        public static void AddFolderRule() { 
+            
+        }
+
+        public static void TestViewCases()
+        {
+            //passedState = (bool)testCases[0].caseFunction.DynamicInvoke();
+
+            foreach (ViewEnginePlusCase testCase in testCases)
+            {
+                if ((bool)testCase.caseFunction.DynamicInvoke())
+                {
+                    // it has passed so lets break out and set var
+
+                    viewCase = testCase.name;
+                    break;
+                }
+            }
+        }
+
+
+        private class ViewEnginePlusCase
+        {
+            public String name;
+            public Delegate caseFunction;
+        }
     }
+    
 }
