fc85e9614c6e3bdac5e0f06bfbf08fc9c2980511
Aaron layton <aaron.r.layton@gmail.com>
2014-02-27 20:51:22 +0000

    Added more views for testing with

---
+++ ViewEnginePlus.Demo/Controllers/ThemesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewEnginePlus.Demo.Controllers
+{
+    public class ThemesController : Controller
+    {
+        //
+        // GET: /Themes/
+
+        public ActionResult Index()
+        {
+            // This is trying to default to the /Themes folder?
+            return View();
+        }
+
+    }
+}
